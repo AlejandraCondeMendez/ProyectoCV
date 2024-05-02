8b53ee637d3e7b0031c3d09c2ea603cde7637bdf
AlejandraCondeMendez <168209868+AlejandraCondeMendez@users.noreply.github.com>
2024-05-02 11:59:39 -0600

    Add files via upload

---
+++ Proyecto A/ProyectoA/proyectoA.js
@@ -0,0 +1,31 @@
+let cirResolucion = document.getElementById("cirResolucion")
+
+
+cirResolucion.addEventListener("click",()=>{
+    alert("Soy buena solucionando conflictos laborales. Gracias a mi capacidad de análisis.")
+})
+
+
+let cirComunicacion = document.getElementById("cirComunicacion")
+cirComunicacion.addEventListener("click", ()=>{
+    alert("Tengo la destreza de transmitir ideas e información de manera asertiva a otras personas.")
+})
+
+let cirCreatividad = document.getElementById("cirCreatividad")
+cirCreatividad.addEventListener("click", ()=>{
+    alert("Tengo la capacidad mental de fomentar ideas innovadoras como también consideración a diversos enfoques para abordar problemas.")
+})
+
+let cirTrabajo = document.getElementById("cirTrabajo")
+cirTrabajo.addEventListener("click", () =>{
+    alert("Tengo la capacidad de trabajar en equipo como una unidad para llegar a los objetivos planteados y así fomentar la productividad y eficiencia.")
+})
+
+let cirTolerancia = document.getElementById("cirTolerancia")
+cirTolerancia.addEventListener("click", () =>{
+    alert("Tengo la habilidad de seguir actuando con eficacia en situaciones de presión de tiempo, desacuerdos, oposiciones, etc.")
+})
+
+
+
+
